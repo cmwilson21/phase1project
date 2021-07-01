d47b8cd0efee574d86083eb8c986308c45602f1c
Courtney Wilson <cmwilson132@gmail.com>
2021-07-01 16:45:42 -0500

    changes before third try

--- index.js
+++ index.js
@@ -9,41 +9,61 @@
 
 //Node Getters
 //const characterDisplay = document.getElementById("display");
-const books = document.getElementById("books");
+// const books = document.getElementById("books");
+const getForm = () => document.getElementById("form");
+const getSelect = () => document.getElementById("select-books");
+const displayCharacter = () => document.getElementById("display");
 
 // Event Listeners
-document.getElementById('books').addEventListener('mouseover', function() {
-    document.getElementById('books').style.backgroundColor = "white";
+document.addEventListener('DOMContentLoaded',() => {
+    getForm().addEventListener('submit', submitForm);
+})
+
+document.getElementById('select-books').addEventListener('mouseover', function() {
+    document.getElementById('select-books').style.backgroundColor = "white";
 });
 
-document.getElementById('books').addEventListener('mouseout', function() {
-    document.getElementById('books').style.backgroundColor = "rgb(179, 177, 177)";
+document.getElementById('select-books').addEventListener('mouseout', function() {
+    document.getElementById('select-books').style.backgroundColor = "rgb(179, 177, 177)";
 });
 
 
 //Functions
 // function show(books) {
 //     const display = document.getElementById("display");
-//     display.innerHTML = books.option[books.selectedIndex].json[0];
+//     display.innerHTML = books.option[books.selectedIndex[1]].data[0].appendChild(display);
 // }
 
 // const obj = JSON.parse();
 // document.getElementById("display").innerHTML =
 // obj.name + obj.title + obj.description;
 
+const submitForm = e => {
+    e.preventDefault();
+    let title = getSelect().value;
+    fetch('http://localhost:3000/characters')
+    .then(respond => respond.json())
+    .then(characters => {
+          let filteredCharacters = characters.filter(character => character.books.find(book => title))
+          filteredCharacters.forEach( character => {
+             let p = document.createElement('p');
+             
+             p.innerText = character.name;
+             displayCharacter().appendChild(p);
+             
+            //  p.innerText = character.description;
+          })
+        })
+    }
 
-const url = 'http://localhost:3000/books'
+
+
 
 //Fetch - THIS FUNCTION CONNECTS WITH THE ONE BELOW THAT PROVES JSON
-// fetch(url)
-// .then(function(response) {
-//   return response.json();
-// })
-// .then(function(data){
-//     appendData(data);
-//     console.log(data);
+// .then(function(characters){
+//     let filteredCharacters=characters.filter(character => character.books.find(book =)
 //     document.getElementById('display').innerText=data[0];
-// });
+// })
 
 
 
@@ -54,7 +74,7 @@ const url = 'http://localhost:3000/books'
 
 
 
-//THIS FUNCTION PROVES JSON IS CONNECTED
+//THIS FUNCTION PROVES JSON IS CONNECTED AT THE BOTTOM OF THE PAGE - FOR TESTING
 // function appendData(data) {
 // const mainContainer = document.getElementById("myData");
 // for (let i = 0; i < data.length; i++) {
@@ -67,26 +87,24 @@ const url = 'http://localhost:3000/books'
 
 
 //THIS IS AN ATTEMPT TO POPULATE THE SELECT DROP DOWN WITH JSON DATA
-let dropdown = document.getElementById('books');
-dropdown.length = 0;
 
-let defaultOption = document.createElement('option');
-defaultOption.text = 'Select a Book';
+// let defaultOption = document.createElement('option');
+// defaultOption.text = 'Select a Book';
 
-dropdown.add(defaultOption);
-dropdown.selectedIndex = 0;
+// dropdown.add(defaultOption);
+// dropdown.selectedIndex = 0;
 
-fetch(url)
-    .then(function(response) {
-    return response.json()
-    })
-    .then(function(data) {
-        let option;
+// fetch(url)
+//     .then(function(response) {
+//     return response.json()
+//     })
+//     .then(function(data) {
+//         let option;
 
-        for (let i = 0; i < data.length; i++) {
-            option = document.createElement('option');
-            option.text = data[i].book;
-            option.value = data[i].name;
-            dropdown.add(option);
-        }
-    });
+//         for (let i = 0; i < data.length; i++) {
+//             option = document.createElement('option');
+//             option.text = data[i].book;
+//             option.value = data[i].name;
+//             dropdown.add(option);
+//         }
+//     });
